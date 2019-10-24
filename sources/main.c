248b9029bc26856889d04341375d0e56881cea23
Ygon oldfather Johns <yjohns@ox-u6.21-school.ru>
2019-10-24 14:05:29 +0300

    raed map and piece in string

--- sources/main.c
+++ sources/main.c
@@ -12,44 +12,74 @@
 
 #include "filler.h"
 
-void    read_map(t_map *map, char *line)
-{
-    int	i;
-
-    i = 0;
-	if (!(map->map = (char **)malloc(sizeof(char *) * (map->y + 1))))
-		exit(1);
-	map->map[map->y] = NULL;
-    while(get_next_line(STDIN_FILENO, &line) && !ft_strstr(line, "000 "))
-        free(line);
-    while (i < map->x && get_next_line(STDIN_FILENO, &line))
-	{
-		map->map[map->y] = ft_strdup(line + 4);
-		free(line);
-	}
-}
-
-void	init_struct(t_filler *f, char *line)
+void	read_map(t_filler *f, char *line)
 {
 	int	w;
 	int	h;
+	int	i;
 
+	i = 0;
 	w = ft_atoi(ft_strchr(line, ' '));
 	h = ft_atoi(ft_strchr(ft_strchr(line, ' ') + 1, ' '));
 	f->board->y = w;
     f->board->x = h;
-
-	printf("y = %d\nx = %d\n", w, h);
-	read_map(f->board, line);
+	if (!(f->board->map = (char **)malloc(sizeof(char *) * (f->board->y + 1))))
+		exit(1);
+	f->board->map[f->board->y] = NULL;
+	while(get_next_line(STDIN_FILENO, &line) && !ft_strstr(line, "0123"))
+		free(line);
+	while (i < f->board->y && get_next_line(STDIN_FILENO, &line))
+	{
+		f->board->map[i] = ft_strdup(line + 4);
+		free(line);
+		i++;
+	}
 }
 
 void	ft_init(t_filler *filler, char *line)
 {
-	filler->player = (char)(line[10] == '1' ? 'O' : 'X');
-	filler->enemy = (char)(line[10] == '1' ? 'X' : 'O');
+	line = ft_strchr(line, 'p');
+	line++;
+	filler->player = (char)(*line == '1' ? 'O' : 'X');
+	filler->enemy = (char)(*line == '1' ? 'X' : 'O');
 	if (!(filler->board = (t_map *)malloc(sizeof(t_map))) ||
 		!(filler->piece = (t_map *)malloc(sizeof(t_map))))
 		exit(1);
+}
+
+void	free_arrays(t_filler *f)
+{
+	int	i;
+
+	i = 0;
+	while (i < f->board->y)
+	{
+		free(f->board->map[i]);
+		i++;
+	}
+	while (i < f->piece->y)
+	{
+		free(f->piece->map[i]);
+		i++;
+	}
+}
+
+void	read_piece(t_filler *f, char *line)
+{
+	int i;
+
+	i = 0;
+	f->piece->y = ft_atoi(ft_strchr(line, ' '));
+	f->piece->x = ft_atoi(ft_strchr(ft_strchr(line, ' ') + 1, ' '));
+	if (!(f->piece->map = (char **)malloc(sizeof(char *) * (f->piece->y + 1))))
+		exit(1);
+	f->piece->map[f->piece->y] = NULL;
+	while (i < f->piece->y && get_next_line(STDIN_FILENO, &line))
+	{
+		f->piece->map[i] = ft_strdup(line);
+		free(line);
+		i++;
+	}
 }
 
 int 	main(void)
@@ -63,12 +93,13 @@ int 	main(void)
 		if ((ft_strstr(line, PLAYER_S) && line[0] == '$'))
 			ft_init(filler, line);
 		else if (ft_strstr(line, "Plateau"))
-			init_struct(filler, line);
-//		else if (ft_strstr(line, "Piece "))
-//		{
+			read_map(filler, line);
+		else if (ft_strstr(line, "Piece "))
+		{
+			read_piece(filler, line);
 //			game();
-//			free_arrays();
-//		}
+			free_arrays(filler);
+		}
 		free(line);
 	}
 	return (0);
